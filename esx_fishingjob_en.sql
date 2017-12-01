12b6debee556dd25097af564209151af77811a39
TanguyOrtegat <tanguy.ortegat@student.odisee.be>
2017-12-01 13:52:02 +0100

    Update esx_fishingjob_en.sql

--- esx_fishingjob_en.sql
+++ esx_fishingjob_en.sql
@@ -2,6 +2,6 @@ use essentialmode
 
 INSERT INTO items (`name`, `label`, `limit`) VALUES
 ('fish', 'Fish', -1),
-('fishing_rod', 'Fishing rod', 2),
+('fishingrod', 'Fishing rod', 2),
 ('bait', 'Bait', 20);
 
