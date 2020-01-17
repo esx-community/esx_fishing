5f985997af9463256ed26aebc638196840eda47a
rex2630 <rex2630@gmail.com>
2020-01-17 21:42:39 +0100

    Removed keys table

--- client/main.lua
+++ client/main.lua
@@ -1,28 +1,13 @@
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
 ESX                    = nil
-local Caught_KEY       = Keys['E']
 local SuccessLimit     = 0.09 -- Maxim 0.1 (high value, low success chances)
 local AnimationSpeed   = 0.0015
 local ShowChatMSG      = true -- or false
 
-local IsFishing        = false
-local CFish            = false
-local BarAnimation     = 0
-local Faketimer        = 0
+local IsFishing, CFish = false, false
+local BarAnimation, Faketimer = 0, 0
 local RunCodeOnly1Time = true
-local PosX             = 0.5
-local PosY             = 0.1
-local TimerAnimation   = 0.1
+local PosX = 0.5
+local PosY, TimerAnimation = 0.1, 0.1
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -184,7 +169,7 @@ Citizen.CreateThread(function()
 				DeleteEntity(FishRod)
 				if ShowChatMSG then ESX.ShowNotification("Le poisson s'est échappé ...") end
 			end
-			if IsControlJustPressed(1, Caught_KEY) then
+			if IsControlJustPressed(1, 38) then
 				if BarAnimation >= SuccessLimit then
 					CFish = false
 					TimerAnimation = 0.1
